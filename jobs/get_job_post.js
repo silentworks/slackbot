ad87c0135d6fe3919a8b73566ce8f59ff45bfce6
Andrew Smith <a.smith@silentworks.co.uk>
2017-05-10 19:01:34 +0000

    Initial start on jobs posting through Google Forms

---
+++ jobs/get_job_post.js
@@ -0,0 +1,10 @@
+/**
+ * Combining response for events into 1 module
+ *
+ * @author Andrew Smith <a.smith@silentworks.co.uk>
+ */
+'use strict';
+
+const config = require('../config');
+const format = require('date-fns/format');
+const sheet = require('google-spreadsheets');
